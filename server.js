ee4d57a2852e886c4a111cc737133edb5f9bdfee
Diego <dpardoblacutt@gmail.com>
2022-08-31 21:02:12 -0400

    Add primary middleware to server.js

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+app.use((req,res,next)=> {
+    console.log(req.path, req.method);
+    next();
+})
+
+
 app.listen(3000, () => {
     console.log("listening on port 3000");
 })
